2911f4e37e3b85168361f8eca648489729499476
hedaukartik <hedaukartik2009@gmail.com>
2018-11-15 23:40:13 +0530

    backout nav

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -22,36 +22,23 @@ class Header extends Component {
             {this.props.branding}
           </a>
           <div>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              onClick={this.onShowMenu}
-              data-target="#navbarText"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
-            {this.state.showHeaderMenu ? (
-              <div className="collapse navbar-collapse show">
-                <ul className="navbar-nav mr-auto">
-                  <li className="nav-item">
-                    <Link to="/" className="nav-link">
-                      Home
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/contact/add" className="nav-link">
-                      Add Contact
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/about" className="nav-link">
-                      About
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            ) : null}
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact/add" className="nav-link">
+                  Add Contact
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about" className="nav-link">
+                  About
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
